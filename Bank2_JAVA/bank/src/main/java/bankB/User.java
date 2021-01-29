33e41052df034099b16280bcefcddcec312518de
LeCarteloo <filippapiernik1999@gmail.com>
2021-01-29 14:38:38 +0100

    Fix

--- Bank2_JAVA/bank/src/main/java/bankB/User.java
+++ Bank2_JAVA/bank/src/main/java/bankB/User.java
@@ -1,17 +1,10 @@
 package bankB;
 
-public class User {
+public class user {
     private String name;
     private String surname;
     private String login;
     private String password;
-    private String pesel;
-    private String email;
-    private String telephoneNumber;
-    private String location;
-    private String street;
-    private String houseNumber;
-    private String postCode;
     private String bankNumber;
     private double balance;
 
@@ -45,62 +38,6 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getPesel() {
-        return pesel;
-    }
-
-    public void setPesel(String pesel) {
-        this.pesel = pesel;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelephoneNumber() {
-        return telephoneNumber;
-    }
-
-    public void setTelephoneNumber(String telephoneNumber) {
-        this.telephoneNumber = telephoneNumber;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getHouseNumber() {
-        return houseNumber;
-    }
-
-    public void setHouseNumber(String houseNumber) {
-        this.houseNumber = houseNumber;
-    }
-
-    public String getPostCode() {
-        return postCode;
-    }
-
-    public void setPostCode(String postCode) {
-        this.postCode = postCode;
     }
 
     public String getBankNumber() {
